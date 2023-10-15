9f76fccfbc9bde3648ba20ea9bc14f4cc9046af6
Qendresa Braha <hiqendresa@gmail.com>
2023-10-15 23:53:31 +0200

    functionality added

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const submitBtn = document.querySelector('.submitBtn');
 const poopup = document.querySelector('.popup');
-const card = doument.querySelector('.container')
+const card = document.querySelector('.container');
+
+let value;
 
 submitBtn.addEventListener('click', () => {
     card.style.display='none';
     poopup.style.display='flex';
+
+    setRating();
 });
 
-console.log('hello there');
+
+const inputs = document.querySelectorAll('input');
+  console.log(inputs);
+
+const getRateValue = () => {
+    inputs.forEach((input) => {
+        input.addEventListener('click', () => value = input.value);
+      
+    })
+} 
+getRateValue();
+
+const setRating = () => {
+    const html = `<div class="rateSelected">You selected <span> ${value} </span> out of 5</div>`
+    document.querySelector('.content p').insertAdjacentHTML('beforebegin', html);
+};
+
