800bb5311efe2e0b489e4d658f0af48644f0820e
Qendresa Braha <91502475+HiQendresa@users.noreply.github.com>
2023-10-16 23:01:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const submitBtn = document.querySelector('.submitBtn');
+const form = document.querySelector('form');
 const poopup = document.querySelector('.popup');
 const card = document.querySelector('.container');
 
 let value;
 
-submitBtn.addEventListener('click', () => {
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     card.style.display='none';
     poopup.style.display='flex';
 
     setRating();
 });
 
-
 const inputs = document.querySelectorAll('input');
-  console.log(inputs);
 
 const getRateValue = () => {
     inputs.forEach((input) => {
         input.addEventListener('click', () => value = input.value);
-      
-    })
+    });
 } 
-getRateValue();
 
 const setRating = () => {
     const html = `<div class="rateSelected">You selected <span> ${value} </span> out of 5</div>`
-    document.querySelector('.content p').insertAdjacentHTML('beforebegin', html);
+    document.querySelector('.popup h2').insertAdjacentHTML('beforebegin', html);
 };
 
+getRateValue();
